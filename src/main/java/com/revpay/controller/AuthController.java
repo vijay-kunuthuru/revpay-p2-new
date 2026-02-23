208b9ae135820d4d47e65be6c6cb13a7fdcba195
Nihir Verma <nihirverma14@gmail.com>
2026-02-23 11:03:47 +0530

    forgot pass feature update

--- src/main/java/com/revpay/controller/AuthController.java
+++ src/main/java/com/revpay/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
