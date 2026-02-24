023d28eab1a9a965efe71bdf8f97c649d043c61d
Nihir Verma <nihirverma14@gmail.com>
2026-02-24 12:10:58 +0530

    loan apply fix

---
+++ src/main/java/com/revpay/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.revpay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    // This perfectly isolates your JPA Auditing!
+}
