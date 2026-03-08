7c585cd6721f813a143ebafa86aea81750d3268d
Vijay <gandlavijay@gmail.com>
2026-03-08 15:32:45 +0530

    added the correct URL in constants

--- RevPay-P2-Frontend/src/app/shared/constants.ts
+++ RevPay-P2-Frontend/src/app/shared/constants.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1'
+  apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:8080/api/v1' : `http://${window.location.hostname}:8080/api/v1`
 };
